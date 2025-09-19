8efe5985d16658a935ca14c295223ba1f66c521c
Klaus <mail@centaurus22.de>
2025-09-19 11:46:46 +0200

    Refactor
    * add comments
    * randomly generate the cell status

--- lib/src/entities/cell.dart
+++ lib/src/entities/cell.dart
@@ -1,4 +1,14 @@
+import 'dart:math';
+
 /// One cell in Conway's game of life
 final class Cell {
-  bool get isAlive => true;
+  //Whether the cell is alive
+  bool _isAlive = false;
+
+  Cell() {
+    _isAlive = Random().nextBool();
+  }
+
+  /// Wether the cell is alive
+  bool get isAlive => _isAlive;
 }
