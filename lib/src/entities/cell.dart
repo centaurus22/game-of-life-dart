e0eacad7e2b32408c5e1722e5c9321215118c211
Klaus <mail@centaurus22.de>
2025-09-19 12:23:59 +0200

    Refactor: add new comments to cell

--- lib/src/entities/cell.dart
+++ lib/src/entities/cell.dart
@@ -1,6 +1,8 @@
 import 'dart:math';
 
 /// One cell in Conway's game of life
+/// 
+/// Setting whether the cell is alive is only for testing purposes
 final class Cell {
   //Whether the cell is alive
   bool isAlive = false;
@@ -9,6 +11,9 @@ final class Cell {
     isAlive = Random().nextBool();
   }
 
+  ///Calculate the live status by setting the current number of live neighbors
+  ///
+  ///A cell can have 0 to 9 live neighbors
   void nextStepCalculate(int neighborsLiving) {
     isAlive = false;
   }
