65c445131a9a1e3b363b27d3ddd5310b44bd7b1d
Klaus <mail@centaurus22.de>
2025-09-19 14:28:20 +0200

    Improve the grid
    * Actually save a list of cells and not bools
    * The cell list should have static size
    * Just pass an Iterable out. It should be sufficient.

--- lib/src/entities/grid.dart
+++ lib/src/entities/grid.dart
@@ -1,12 +1,21 @@
+import 'package:game_of_life_dart/src/entities/cell.dart';
+
 /// The cell grid in Conway's Game of Life
 class Grid {
   //The cells on this grid
-  List<List<bool>> cells = [[]];
+  List<List<Cell>> _cells = [[]];
 
   /// The [height] and [width] can not be negative
   Grid({required int height, required int width}) {
-    cells = List.filled(height, List.filled(width, true));
+    _cells = List.filled(
+      height,
+      List.filled(width, Cell(), growable: false),
+      growable: false
+    );
   }
+  
   /// Get the status of every cell
-  List<List<bool>> get asBools => cells;
+  List<List<bool>> get asBools {
+    return _cells.map((row) => row.map((c) => c.isAlive).toList()).toList();
+  }
 }
