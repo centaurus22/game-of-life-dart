89dc899b369e3c8d0171884c49670ec44177c386
Klaus <mail@centaurus22.de>
2025-09-19 13:59:04 +0200

    Initialize a 2x2 grid

--- lib/src/entities/grid.dart
+++ lib/src/entities/grid.dart
@@ -1,5 +1,10 @@
 /// The cell grid in Conway's Game of Life
 class Grid {
+  List<List<bool>> cells = [[]];
+
+  Grid({required int height, required int width}) {
+    cells = List.filled(height, List.filled(width, true));
+  }
   /// Get the status of every cell
-  List<List<bool>> get asBools => [[true]];
+  List<List<bool>> get asBools => cells;
 }
--- test/src/entities/grid_test.dart
+++ test/src/entities/grid_test.dart
@@ -2,18 +2,20 @@ import 'package:game_of_life_dart/src/entities/grid.dart';
 import 'package:test/test.dart';
 
 void main() {
-  late Grid grid;
-
-  setUp((){
-    grid = Grid();
-  });
-
   test('init_grid', () {
+    var grid = Grid(height: 0, width: 0);
     expect(grid.asBools.runtimeType, List<List<bool>>);
   });
 
   test('init_as_1x1_grid', () {
+    var grid = Grid(height: 1, width: 1);
     expect(grid.asBools.length, 1);
     expect(grid.asBools[0].length, 1);
+  });
+
+  test('init_as_2x3_grid', () {
+    var grid = Grid(height: 2, width: 3);
+    expect(grid.asBools.length, 2);
+    expect(grid.asBools[0].length, 3);
   });
 }
