5a290d2a89a8d95ca6d3e0116f66d22aa9922a47
Klaus <mail@centaurus22.de>
2025-09-19 21:48:09 +0200

    Add an object for positions in the grid

--- lib/src/entities/grid.dart
+++ lib/src/entities/grid.dart
@@ -1,7 +1,8 @@
 import 'package:game_of_life_dart/src/entities/cell.dart';
+import 'package:game_of_life_dart/src/entities/position.dart';
 
 /// The cell grid in Conway's Game of Life
-class Grid {
+final class Grid {
   /// The cells on this grid
   List<List<Cell>> _cells = [[]];
 
@@ -24,7 +25,7 @@ class Grid {
   /// Set the status of every cell. For testing purposes only.
   Grid.cells(List<List<bool>> cells) {
     _cells = cells
-        .map((row) => row.map((c) => Cell.isAlive(c)).toList(growable: false))
+        .map((row) => row.map((b) => Cell.isAlive(b)).toList(growable: false))
         .toList(growable: false);
 
     _height = cells.length;
@@ -45,47 +46,48 @@ class Grid {
         .toList(growable: false);
 
     for (var row = 0; row < _height; row++) {
-      for (var col = 0; col < _width; col++) {
-        _nextStepCalculateOn(row: row, col: col, cellsTemp: cellsTemp);
+      for (var column = 0; column < _width; column++) {
+        var position = Position(row: row, column: column);
+        _nextStepCalculateOn(position: position, cellsTemp: cellsTemp);
       }
     }
   }
 
   void _nextStepCalculateOn({
-    required int row,
-    required int col,
+    required Position position,
     required List<List<Cell>> cellsTemp,
   }) {
     var neighborsLiving =
-        _isAliveAsIntOn(cellsTemp: cellsTemp, row: row - 1, col: col - 1) +
-        _isAliveAsIntOn(cellsTemp: cellsTemp, row: row - 1, col: col) +
-        _isAliveAsIntOn(cellsTemp: cellsTemp, row: row - 1, col: col + 1) +
-        _isAliveAsIntOn(cellsTemp: cellsTemp, row: row, col: col - 1) +
-        _isAliveAsIntOn(cellsTemp: cellsTemp, row: row, col: col + 1) +
-        _isAliveAsIntOn(cellsTemp: cellsTemp, row: row + 1, col: col - 1) +
-        _isAliveAsIntOn(cellsTemp: cellsTemp, row: row + 1, col: col) +
-        _isAliveAsIntOn(cellsTemp: cellsTemp, row: row + 1, col: col + 1);
+        _isAliveAsIntOn(position: position.uL(), cellsTemp: cellsTemp) +
+        _isAliveAsIntOn(position: position.uC(), cellsTemp: cellsTemp) +
+        _isAliveAsIntOn(position: position.uR(), cellsTemp: cellsTemp) +
+        _isAliveAsIntOn(position: position.mL(), cellsTemp: cellsTemp) +
+        _isAliveAsIntOn(position: position.mR(), cellsTemp: cellsTemp) +
+        _isAliveAsIntOn(position: position.lL(), cellsTemp: cellsTemp) +
+        _isAliveAsIntOn(position: position.lC(), cellsTemp: cellsTemp) +
+        _isAliveAsIntOn(position: position.lR(), cellsTemp: cellsTemp);
 
-    _cells[row][col].nextStepCalculate(neighborsLiving: neighborsLiving);
+    _cells[position.row][position.column].nextStepCalculate(
+      neighborsLiving: neighborsLiving,
+    );
   }
 
   int _isAliveAsIntOn({
-    required int row,
-    required int col,
+    required Position position,
     required List<List<Cell>> cellsTemp,
   }) {
-    if (row == -1) {
-      row = _height - 1;
-    } else if (row == _height) {
-      row = 0;
+    if (position.row == -1) {
+      position.row = _height - 1;
+    } else if (position.row == _height) {
+      position.row = 0;
     }
 
-    if (col == -1) {
-      col = _width - 1;
-    } else if (col == _width) {
-      col = 0;
+    if (position.column == -1) {
+      position.column = _width - 1;
+    } else if (position.column == _width) {
+      position.column = 0;
     }
 
-    return cellsTemp[row][col].isAlive ? 1 : 0;
+    return cellsTemp[position.row][position.column].isAlive ? 1 : 0;
   }
 }
--- lib/src/entities/position.dart
+++ lib/src/entities/position.dart
@@ -4,4 +4,13 @@ final class Position {
   int column;
 
   Position({required this.row, required this.column});
+
+  Position uL() => Position(row: row - 1, column: column - 1);
+  Position uC() => Position(row: row - 1, column: column);
+  Position uR() => Position(row: row - 1, column: column + 1);
+  Position mL() => Position(row: row, column: column - 1);
+  Position mR() => Position(row: row, column: column + 1);
+  Position lL() => Position(row: row + 1, column: column - 1);
+  Position lC() => Position(row: row + 1, column: column);
+  Position lR() => Position(row: row + 1, column: column + 1);
 }
