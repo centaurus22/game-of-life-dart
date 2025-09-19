828c078a777fe86c6f9944730a5d96ae9b59111c
Klaus <mail@centaurus22.de>
2025-09-19 12:39:04 +0200

    Refactor
    * Fix comment style
    * Fix info: A cell has only eight neighbors

--- lib/src/entities/cell.dart
+++ lib/src/entities/cell.dart
@@ -4,16 +4,16 @@ import 'dart:math';
 /// 
 /// Setting whether the cell is alive is only for testing purposes
 final class Cell {
-  //Whether the cell is alive
+  /// Whether the cell is alive
   bool isAlive = false;
 
   Cell() {
     isAlive = Random().nextBool();
   }
 
-  ///Calculate the live status by setting the current number of live neighbors
+  /// Calculate the live status by setting the current number of live neighbors
   ///
-  ///A cell can have 0 to 9 live neighbors
+  /// A cell can have 0 to 8 live neighbors
   void nextStepCalculate(int neighborsLiving) {
     if (neighborsLiving >= 2) {
       isAlive = true;
