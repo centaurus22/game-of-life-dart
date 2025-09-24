72a3965ff4f1a1e8090ac08414ce2b6dff752701
Klaus <mail@centaurus22.de>
2025-09-24 14:10:58 +0200

    Add comments to initialize use case

--- lib/use_cases/initialize.dart
+++ lib/use_cases/initialize.dart
@@ -3,8 +3,15 @@ import '../src/entities/grid.dart';
 
 import 'initialize_interface.dart';
 
-class Initialize extends InitializeInterface {
+/// Initialize the Grid
+final class Initialize extends InitializeInterface {
   @override
+  /// Initialize the Grid
+  /// 
+  /// @param dimensions Dimensions of the new [Grid] as Map.
+  /// @throws an ArgumentError if the Map do not contains values
+  ///   for height or width.
+  /// @return the new [Grid].
   Grid execute(Map<String, int> dimensions) {
     if (!dimensions.containsKey('x')) {
       throw ArgumentError('No width provided (Map key "x")');
--- lib/use_cases/initialize_interface.dart
+++ lib/use_cases/initialize_interface.dart
@@ -1,5 +1,10 @@
 import '../src/entities/grid.dart';
 
+/// Initialize the Grid
 abstract class InitializeInterface {
+  /// Initialize the Grid
+  /// 
+  /// @param dimensions Dimensions of the new [Grid] as Map.
+  /// @return the new [Grid].
   Grid execute(Map<String, int> dimensions);
 }
