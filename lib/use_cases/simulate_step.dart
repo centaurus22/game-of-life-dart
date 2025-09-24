5b4958d7f91b9d6479f79fa55615e3611a73c089
Klaus <mail@centaurus22.de>
2025-09-24 14:17:26 +0200

    Add doc comments to the simulate  use case

--- lib/use_cases/simulate_step.dart
+++ lib/use_cases/simulate_step.dart
@@ -1,6 +1,11 @@
 import 'package:game_of_life_dart/src/entities/grid.dart';
 
+//Simulate a step
 final class SimulateStep {
+  /// Simulate a step.
+  /// 
+  /// @param The current [Grid].
+  /// @return The next step of the [Grid].
   Grid execute(Grid grid) {
     return grid;
   }
