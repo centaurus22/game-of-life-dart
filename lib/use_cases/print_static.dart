45183f4ddce78cc46c4a004002f50741fe758018
Klaus <mail@centaurus22.de>
2025-09-26 09:12:50 +0200

    Pass display adapter by dependency injection in PrintStatic use case

--- lib/use_cases/print_static.dart
+++ lib/use_cases/print_static.dart
@@ -1,6 +1,13 @@
+import 'display_adapter_interface.dart';
 import 'print_static_interface.dart';
 
 class PrintStatic extends PrintStaticInterface {
+  final DisplayAdapterInterface displayAdapter;
+
+  PrintStatic(this.displayAdapter);
+
   @override
-  void execute() {}
+  void execute() {
+    displayAdapter.printStatic();
+  }
 }
--- test/use_cases/print_static_test.dart
+++ test/use_cases/print_static_test.dart
@@ -1,12 +1,11 @@
 import 'package:game_of_life_dart/use_cases/print_static.dart';
 import 'package:test/test.dart';
 
+import 'mock_display_adapter.dart';
+
 void main() {
   test('print_static_elements', () {
-    final printStatic = PrintStatic();
-    expect(
-      () => printStatic.execute(),
-      returnsNormally,
-    );
+    final printStatic = PrintStatic(MockDisplayAdapter());
+    expect(() => printStatic.execute(), returnsNormally);
   });
 }
