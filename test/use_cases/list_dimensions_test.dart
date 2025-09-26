e26d90bab6c4ad61c2c71ecb908f281c8cb1992f
Klaus <mail@centaurus22.de>
2025-09-26 08:56:05 +0200

    Move MockDisplayAdapter to own file

--- test/use_cases/list_dimensions_test.dart
+++ test/use_cases/list_dimensions_test.dart
@@ -1,17 +1,9 @@
 import 'package:game_of_life_dart/src/entities/coordinates.dart';
-import 'package:game_of_life_dart/use_cases/display_adapter_interface.dart';
 import 'package:game_of_life_dart/use_cases/list_dimensions.dart';
 import 'package:test/test.dart';
 
-class MockDisplayAdapter extends DisplayAdapterInterface {
-  @override
-  Dimensions listDimensions() {
-    return Dimensions.set(x: 80, y: 25);
-  }
+import 'mock_display_adapter.dart';
 
-  @override
-  void printStatic(Dimensions _) {}
-}
 
 void main() {
   test('get terminal dimensions', () {
---
+++ test/use_cases/mock_display_adapter.dart
@@ -0,0 +1,12 @@
+import 'package:game_of_life_dart/src/entities/coordinates.dart';
+import 'package:game_of_life_dart/use_cases/display_adapter_interface.dart';
+
+class MockDisplayAdapter extends DisplayAdapterInterface {
+  @override
+  Dimensions listDimensions() {
+    return Dimensions.set(x: 80, y: 25);
+  }
+
+  @override
+  void printStatic() {}
+}
