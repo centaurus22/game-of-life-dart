33a56bbf10f84188ee3f480219c0e7bb4b13bb7c
Klaus <mail@centaurus22.de>
2025-09-30 18:45:00 +0200

    Add use cases to main lib object

--- lib/game_of_life_dart.dart
+++ lib/game_of_life_dart.dart
@@ -1,1 +1,51 @@
+import 'src/entities/coordinates.dart';
+import 'src/entities/grid.dart';
+import 'use_cases/display_adapter_interface.dart';
+import 'use_cases/draw_static.dart';
+import 'use_cases/draw_static_interface.dart';
+import 'use_cases/initialize.dart';
+import 'use_cases/initialize_interface.dart';
+import 'use_cases/list_dimensions.dart';
+import 'use_cases/list_dimensions_interface.dart';
+import 'use_cases/set_up_display.dart';
+import 'use_cases/set_up_display_interface.dart';
+import 'use_cases/simulate_step.dart';
+import 'use_cases/simulate_step_interface.dart';
+import 'use_cases/tear_down_display.dart';
+import 'use_cases/tear_down_display_interface.dart';
 
+class GameOfLifeDart {
+  final DisplayAdapterInterface _displayAdapter;
+
+  GameOfLifeDart(this._displayAdapter);
+
+  Dimensions listDimensions() {
+    final ListDimensionsInterface useCase = ListDimensions(_displayAdapter);
+    return useCase.execute();
+  }
+
+  Grid initialize(Dimensions dimensions) {
+    final InitializeInterface useCase = Initialize();
+    return useCase.execute(dimensions);
+  }
+
+  void setUpDisplay() {
+    final SetUpDisplayInterface useCase = SetUpDisplay(_displayAdapter);
+    useCase.execute();
+  }
+
+  void drawStatic() {
+    final DrawStaticInterface useCase = DrawStatic(_displayAdapter);
+    useCase.execute();
+  }
+
+  void simulateStep(Grid grid) {
+    final SimulateStepInterface useCase = SimulateStep();
+    useCase.execute(grid);
+  }
+
+  void tearDownDisplay() {
+    final TearDownDisplayInterface useCase = TearDownDisplay(_displayAdapter);
+    useCase.execute();
+  }
+}
