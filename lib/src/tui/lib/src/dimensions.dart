f658d7a7ab5af7b5490bc95d037da70410df8798
Klaus <thorres@brothersofgrey.net>
2025-10-23 09:59:55 +0200

    Change width and height in dimesion
    Because in every other dimension like object and parameter lst the x variable is defined befor the y variable.

--- lib/src/tui/lib/src/dimensions.dart
+++ lib/src/tui/lib/src/dimensions.dart
@@ -1,11 +1,11 @@
 /// Dimensions of the screen
 class Dimensions {
-  /// Height of the screen
-  final int height;
-
   /// Width of the screen
   final int width;
 
+  /// Height of the screen
+  final int height;
+
   /// Initialize the object
-  Dimensions({required this.height, required this.width});
+  Dimensions({required this.width, required this.height});
 }
